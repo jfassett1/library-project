45e35579b17a4a3484abe63cb65f18eeb14d1eb8
jamorr <55964176+jamorr@users.noreply.github.com>
2023-11-01 13:03:20 -0400

    removed sqllite db

--- library_project/library_project/settings.py
+++ library_project/library_project/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'library_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-app-db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
